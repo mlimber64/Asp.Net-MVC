c260af342855c9929db20627f562a24bef9cb681
Limber Mendoza <mlimber64@gmail.com>
2023-05-18 10:48:42 -0500

    update PeriodoGradoCursoController

---
+++ WebApplication1/Controllers/PeriodoGCursoController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class PeriodoGCursoController : Controller
+    {
+        // GET: PeriodoGCurso
+        PruebaDataContext db = new PruebaDataContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult findAll()
+        {
+            var lista = (from pgc in db.PeriodoGradoCurso
+                         join per in db.Periodo
+                         on pgc.IIDPERIODO equals per.IIDPERIODO
+                         join grad in db.Grado
+                         on pgc.IIDGRADO equals grad.IIDGRADO
+                         join cur in db.Curso
+                         on pgc.IIDCURSO equals cur.IIDCURSO
+
+                         select new
+                         {
+                             pgc.IID,
+                             NOMBREÉRIODO = per.NOMBRE,
+                             NOMBREGRADO = grad.NOMBRE,
+                             NOMBRECURSO = cur.NOMBRE
+                         }).ToList();
+
+            return Json(lista, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult recuperarInfo(int id_pgc)
+        {
+            var lista = db.PeriodoGradoCurso.Where(p => p.IID.Equals(id_pgc)).
+                Select(p => new
+                {
+                    p.IID,
+                    p.IIDPERIODO,
+                    p.IIDGRADO,
+                    p.IIDCURSO
+                });
+
+            return Json(lista, JsonRequestBehavior.AllowGet);
+
+        }
+
+        public JsonResult comboPeriodo()
+        {
+            var lista = db.Periodo.Where(p => p.BHABILITADO.Equals(1)).
+                Select(p => new
+                {
+                    IID = p.IIDPERIODO,
+                    p.NOMBRE
+                });
+
+            return Json(lista, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult comboGrado()
+        {
+            var lista = db.Grado.Where(p => p.BHABILITADO.Equals(1)).
+                Select(p => new
+                {
+                    IID = p.IIDGRADO,
+                    p.NOMBRE
+                });
+
+            return Json(lista, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult comboCurso()
+        {
+            var lista = db.Curso.Where(p => p.BHABILITADO.Equals(1)).
+                Select(p => new
+                {
+                    IID = p.IIDCURSO,
+                    p.NOMBRE
+                });
+
+            return Json(lista, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ WebApplication1/Scripts/periodogradocurso.js
@@ -0,0 +1,155 @@
+﻿
+function listar() {
+
+    $.get("findAll/PeriodoGCurso", function (data) {
+
+        crearListado(["ID", "PERIODO", "GRADO", "CURSO"], data);
+    })
+
+    $.get("comboPeriodo/PeriodoGradoCurso", function (data) {
+
+        llenarCombo(data, document.getElementById("cboPeriodo"), true);
+    })
+
+    $.get("comboGrado/PeriodoGCurso", function (data) {
+
+        llenarCombo(data, document.getElementById("cboGrado"), true);
+    })
+
+    $.get("comboCurso/PeriodoGCurso", function (data) {
+
+        llenarCombo(data, document.getElementById("cboCurso"), true);
+    })
+}
+
+listar();
+
+
+function crearListado(arrayColumnas, data) {
+
+    var contenido = "";
+
+    contenido += "<table id='tabla-pgc' class='table table-bordered table-striped'>";
+    contenido += "<thead>";
+    contenido += "<tr>";
+    for (var i = 0; i < arrayColumnas.length; i++) {
+
+        contenido += "<td>"
+
+        contenido += arrayColumnas[i];
+
+        contenido += "</td>"
+    }
+    contenido += "<td>Acciones</td>"
+    contenido += "</tr>";
+    contenido += "</thead>";
+
+    var llaves = Object.keys(data[0]);
+
+
+    contenido += "<tbody>";
+
+    for (var i = 0; i < data.length; i++) {
+
+        contenido += "<tr>";
+        for (var j = 0; j < llaves.length; j++) {
+
+            var valorLlaves = llaves[j];
+
+            contenido += "<td>";
+
+            contenido += data[i][valorLlaves];
+
+            contenido += "</td>";
+
+        }
+
+        var llaveId = llaves[0];
+
+        contenido += "<td>";
+        contenido += "<button class='btn btn-primary' onclick='abrirModal(" + data[i][llaveId] + ")' data-toggle='modal' data-target='#myModal'><i class='glyphicon glyphicon-edit'></i></button> ";
+        contenido += "<button class='btn btn-danger' onclick='eliminar(" + data[i][llaveId] + ")'><i class='glyphicon glyphicon-trash'></i></button>";
+        contenido += "</td>";
+        contenido += "</tr>";
+    }
+
+    contenido += "</tbody>";
+    contenido += "</table>";
+    document.getElementById("tabla").innerHTML = contenido;
+    $("#tabla-pgc").dataTable({
+
+        searching: false
+    });
+}
+
+function llenarCombo(data, control, primerElemento) {
+
+    var contenido = "";
+
+    if (primerElemento == true) {
+
+        contenido += "<option value=''>--Seleccione--</option>";
+    }
+
+    for (var i = 0; i < data.length; i++) {
+
+        contenido += "<option value='" + data[i].IID + "'>";
+
+        contenido += data[i].NOMBRE;
+
+        contenido += "</option>";
+    }
+    control.innerHTML = contenido;
+}
+
+function datosObligatorios() {
+
+    var exito = true;
+    var controlesObligarios = document.getElementsByClassName("obligatorio");
+    var ncontroles = controlesObligarios.length;
+    for (var i = 0; i < ncontroles; i++) {
+
+        if (controlesObligarios[i].value == "") {
+            exito = false;
+            controlesObligarios[i].parentNode.classList.add("error");
+        } else {
+            controlesObligarios[i].parentNode.classList.remove("error");
+        }
+    }
+    return exito;
+}
+
+function borrarDatos() {
+
+    var controles = document.getElementsByClassName("borrar");
+    var ncontroles = controles.length;
+    for (var i = 0; i < ncontroles; i++) {
+
+        controles[i].value = "";
+    }
+}
+
+function abrirModal(id) {
+
+    var controlesObligarios = document.getElementsByClassName("obligatorio");
+    var ncontroles = controlesObligarios.length;
+
+    for (var i = 0; i < ncontroles; i++) {
+        controlesObligarios[i].parentNode.classList.remove("error");
+    }
+
+    if (id == 0) {
+        borrarDatos();
+
+    } else {
+
+        $.get("recuperarInfo/PeriodoGCurso/?id_pgc = " + id, function (data) {
+            document.getElementById("txtId").value = data[0].IID;
+            document.getElementById("cboPeriodo").value = data[0].IIDPERIODO;
+            document.getElementById("cboGrado").value = data[0].IIDGRADO;
+            document.getElementById("cboCurso").value = data[0].IIDCURSO;
+
+        });
+    }
+
+}
